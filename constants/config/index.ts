e3324dff6c600afcde58952c2106e467fa8bdb5b
dsoldatov <danila.soldatov@mercurydevelopment.com>
2024-08-05 22:02:08 +0600

    feat: add rn config, set up config constants

---
+++ constants/config/index.ts
@@ -0,0 +1,7 @@
+import Config from 'react-native-config';
+
+export const CONFIG = {
+  TWITCH_CLIENT_ID: Config.TWITCH_CLIENT_ID,
+  TWITCH_AUTH_URL: Config.TWITCH_AUTH_URL,
+  REDIRECT_URI: Config.REDIRECT_URI,
+};
